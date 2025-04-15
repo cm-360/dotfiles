1d720f2b36c6e5d8979ce1904fd673bbca15166b
CM360 <31625347+cm-360@users.noreply.github.com>
2025-04-14 22:48:44 -0400

    chore: add/update firefox addons

--- nix/home-manager/modules/firefox/addons/update-addons.py
+++ nix/home-manager/modules/firefox/addons/update-addons.py
@@ -5,16 +5,32 @@ from functools import reduce
 from typing import Any
 
 addons = {
+    "auto-tab-discard": "auto-tab-discard",
+    "better-canvas": "better-canvas",
+    "better-darker-docs": "better-darker-docs",
     "bitwarden": "bitwarden-password-manager",
+    "buster-captcha-solver": "buster-captcha-solver",
+    "canvasblocker": "canvasblocker",
     "clearurls": "clearurls",
     "darkreader": "darkreader",
+    "disable-page-visibility": "disable-page-visibility",
+    "download-with-jdownloader": "download-with-jdownloader",
+    "fastforward": "fastforwardteam",
+    "indie-wiki-buddy": "indie-wiki-buddy",
+    "librezam": "librezam",
     "localcdn": "localcdn-fork-of-decentraleyes",
     "plasma-integration": "plasma-integration",
     "privacy-badger": "privacy-badger17",
+    "react-devtools": "react-devtools",
+    "return-youtube-dislike": "return-youtube-dislikes",
     "simple-tab-groups": "simple-tab-groups",
     "stylus": "styl-us",
+    "tampermonkey": "tampermonkey",
+    "temporary-containers": "temporary-containers",
     "terms-of-service-didnt-read": "terms-of-service-didnt-read",
     "ublock-origin": "ublock-origin",
+    "user-agent-string-switcher": "user-agent-string-switcher",
+    "youtube-for-tv": "youtube-for-tv",
 }
 
 
