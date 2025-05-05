aa47f08256461464d531612582aa8f9ad291fd29
CM360 <31625347+cm-360@users.noreply.github.com>
2025-05-05 03:39:18 -0400

    feat: install oh-my-zsh during dotfiles setup

--- install.sh
+++ install.sh
@@ -14,6 +14,12 @@ else
     git pull
 fi
 
+# Install oh-my-zsh
+if which zsh > /dev/null && [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+    echo "⚠️ Oh My Zsh is already installed, skipping."
+fi
 
 ./update.sh
 
