05a654451db984730152920ec486253d872eacbd
CM360 <31625347+cm-360@users.noreply.github.com>
2025-05-08 18:58:02 +0000

    style(nixhm/orion): shrink home manager session var comment

--- nix/home-manager/homes/orion_cm360/home.nix
+++ nix/home-manager/homes/orion_cm360/home.nix
@@ -50,18 +50,10 @@
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
   # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/cm360/etc/profile.d/hm-session-vars.sh
-  #
+  # located at one of:
+  # - ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  # - ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  # - /etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
