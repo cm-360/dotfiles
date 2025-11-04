35a48c6102ed848a8d1ad4b589b50a0ab4462398
CM360 <31625347+cm-360@users.noreply.github.com>
2025-11-04 02:30:24 -0500

    fix(nixos): remove unnecessary libvirt OVMF config

--- nix/nixos/modules/libvirt.nix
+++ nix/nixos/modules/libvirt.nix
@@ -7,15 +7,6 @@
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd
-        ];
-      };
 
       # https://discourse.nixos.org/t/virt-manager-cannot-find-virtiofsd/26752
       vhostUserPackages = with pkgs; [ virtiofsd ];
