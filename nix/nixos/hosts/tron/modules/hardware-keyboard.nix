f8a77bb836c58ed42c4284357103bfc6878c28ac
CM360 <31625347+cm-360@users.noreply.github.com>
2025-12-01 01:50:43 -0500

    fix(nixos/tron): fix keyboard remappings and add docs

--- nix/nixos/hosts/tron/modules/hardware-keyboard.nix
+++ nix/nixos/hosts/tron/modules/hardware-keyboard.nix
@@ -1,11 +1,38 @@
 {
-  # https://wiki.archlinux.org/title/Keyboard_input#Identifying_scancodes
-  # https://wiki.archlinux.org/title/Map_scancodes_to_keycodes#Using_udev
+  # ----- References -----
+  #
+  # Arch Wiki:
+  # - https://wiki.archlinux.org/title/Map_scancodes_to_keycodes
+  # - https://wiki.archlinux.org/title/Keyboard_input#Identifying_scancodes
+  # - https://wiki.archlinux.org/title/Udev#Remap_specific_device
+  #
+  # Keycode name lists:
+  # - https://hal.freedesktop.org/quirk/quirk-keymap-list.txt
+  # - https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
+  #
+  # hwdb manual:
+  # - https://www.man7.org/linux/man-pages/man7/hwdb.7.html
+  #
+  # ----- How to Remap -----
+  #
+  # 1. Identify device's evdev path:
+  #   - Run `evtest` -> format: "/dev/input/eventX"
+  #
+  # 2. Identify keyboard scancodes:
+  #   - Run `evtest /dev/input/eventX`
+  #   - Check `MSC_SCAN` event values
+  #
+  # 3. Map scancodes to keycodes:
+  #   - Use hwdb entries with KEYBOARD_KEY_<scancode>=<keycode>
+  #
+  # Optionally, identify device's modalias:
+  #   - Run `cat /sys/class/input/eventX/device/modalias`
+  #   - Format: "input:bXXXXvXXXXpXXXXeXXXX"
 
-  # AT Translated Set 2 keyboard
-  # /dev/input/event0
   services.udev.extraHwdb = ''
-    evdev:input:b0011v0001p0001eAB83*
+    # /dev/input/event0: AT Translated Set 2 keyboard
+    # evdev:input:b0011v0001p0001eAB83*
+    evdev:name:AT Translated Set 2 keyboard:*
      KEYBOARD_KEY_92=previoussong
      KEYBOARD_KEY_93=playpause
      KEYBOARD_KEY_94=nextsong
