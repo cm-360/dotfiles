e84aaf5e6d6a4d172fe607c5ec4c9421aaa2e6f1
CM360 <31625347+cm-360@users.noreply.github.com>
2025-12-02 15:48:12 -0500

    feat(scripts): add various nix command aliases

--- scripts/shell/aliases.sh
+++ scripts/shell/aliases.sh
@@ -22,9 +22,21 @@ alias fastfetch="fastfetch -c paleofetch.jsonc"
 alias adb-shizuku="adb shell /data/app/~~xcFWMucY7HgQlOOf7yyoGw==/moe.shizuku.privileged.api-UviotepBCAYoAPXjk7LDmA==/lib/arm64/libshizuku.so"
 alias adb-automate="adb shell sh /sdcard/Android/data/com.llamalab.automate/cache/start.sh"
 
+# Home-Manager
+alias hm="home-manager"
+# Nix/NixOS
+alias nf="nix flake"
+alias nrb="nixos-rebuild"
+alias ncg="nix-collect-garbage"
+
 nix-run() {
     pkg=$1
     shift
     nix run nixpkgs#"$pkg" -- "$@"
 }
 
+# https://github.com/getsops/sops/issues/1999#issuecomment-3554147675
+sops-host() {
+    SOPS_AGE_KEY=$(sudo ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key) \
+    sops "$@"
+}
