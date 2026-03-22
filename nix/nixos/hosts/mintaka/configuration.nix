83dd35c94b02e85bb7771f823184a94391b8e5aa
CM360 <31625347+cm-360@users.noreply.github.com>
2026-03-21 21:42:03 -0400

    feat(nixos/mintaka): configure mintaka for remote nix builds

--- nix/nixos/hosts/mintaka/configuration.nix
+++ nix/nixos/hosts/mintaka/configuration.nix
@@ -1,9 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ pkgs, lib, ... }:
-
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 {
   imports = [
     # Include the results of the hardware scan.
@@ -17,6 +17,10 @@
     ./modules/boot.nix
     ./modules/hardware-nvidia.nix
   ];
+
+  nix.settings = {
+    inherit (inputs.secrets.nix) trusted-public-keys;
+  };
 
   nixpkgs.config.allowUnfreePredicate =
     pkg:
